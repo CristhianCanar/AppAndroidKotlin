59fd6f6a2d3370f3a720d93295551445546e5922
CristhianCanar <cristhian.canar.m@gmail.com>
2024-05-28 09:50:51 -0500

    Cambio de version Hilt, se inyecta retrofit, no funciona la app

--- app/src/main/java/com/cristhian/apprecordar/core/di/NetworkModule.kt
+++ app/src/main/java/com/cristhian/apprecordar/core/di/NetworkModule.kt
@@ -1,12 +1,14 @@
 package com.cristhian.apprecordar.core.di
 
 import com.cristhian.apprecordar.core.RetrofitHelper
+import com.cristhian.apprecordar.data.network.clients.QuoteApiClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -22,5 +24,11 @@ object NetworkModule {
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideQuoteApiClient(retrofit: Retrofit): QuoteApiClient {
+        return retrofit.create(QuoteApiClient::class.java)
     }
 }
--- app/src/main/java/com/cristhian/apprecordar/data/network/services/QuoteService.kt
+++ app/src/main/java/com/cristhian/apprecordar/data/network/services/QuoteService.kt
@@ -1,6 +1,5 @@
 package com.cristhian.apprecordar.data.network.services
 
-import com.cristhian.apprecordar.core.RetrofitHelper
 import com.cristhian.apprecordar.data.model.QuoteModel
 import com.cristhian.apprecordar.data.network.clients.QuoteApiClient
 import kotlinx.coroutines.Dispatchers
@@ -8,12 +7,13 @@ import kotlinx.coroutines.withContext
 import retrofit2.Response
 import javax.inject.Inject
 
-class QuoteService @Inject constructor() {
-    private val retrofit = RetrofitHelper.getRetrofit()
-
+class QuoteService @Inject constructor(
+    private val api:QuoteApiClient
+) {
     suspend fun getQuotes():List<QuoteModel>{
         return withContext(Dispatchers.IO){
-            val response:Response<List<QuoteModel>> = retrofit.create(QuoteApiClient::class.java).getAllQuotes()
+            //val response:Response<List<QuoteModel>> = retrofit.create(QuoteApiClient::class.java).getAllQuotes()
+            val response:Response<List<QuoteModel>> = api.getAllQuotes()
             response.body() ?: emptyList()
         }
     }
